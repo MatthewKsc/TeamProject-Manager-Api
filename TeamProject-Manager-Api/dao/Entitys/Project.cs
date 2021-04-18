150192f8883c82e64a0ec01a0bba3f962b798c07
ksciukmateusz <ksciukmateusz@gmail.com>
2021-04-18 17:43:37 +0200

    Project entity Implementation

--- TeamProject-Manager-Api/dao/Entitys/Project.cs
+++ TeamProject-Manager-Api/dao/Entitys/Project.cs
@@ -5,5 +5,30 @@ using System.Threading.Tasks;
 
 namespace TeamProject_Manager_Api.dao.Entitys {
     public class Project {
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime? StartOfProject { get; set; }
+        public DateTime? EndOfProject { get; set; }
+        public virtual ICollection<User> UsersAssigned { get; set; }
+        public int OwnerTeamId { get; set; }
+        public virtual Team OwnerTeam{ get; set; }
+
+        public Project() {
+
+        }
+
+        public Project(string title, string desc, ICollection<User> users, Team team) {
+            this.Title = title;
+            this.Description = desc;
+            this.UsersAssigned = users;
+            this.OwnerTeam = team;
+        }
+
+        public void SetDatesOfProject(DateTime start, DateTime end) {
+            this.StartOfProject = start;
+            this.EndOfProject = end;
+        }
     }
 }
