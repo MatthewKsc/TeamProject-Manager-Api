2900d4691cc89501edc02cbcc2acd1a186cc8c69
ksciukmateusz <ksciukmateusz@gmail.com>
2021-04-18 16:58:36 +0200

    Company Entity Implementation

--- TeamProject-Manager-Api/dao/Entitys/Company.cs
+++ TeamProject-Manager-Api/dao/Entitys/Company.cs
@@ -6,6 +6,21 @@ using System.Threading.Tasks;
 namespace TeamProject_Manager_Api.dao.Entitys {
     public class Company {
 
-        
+        public int Id { get; set; }
+        public string CompanyName { get; set; }
+        public int SizeOfComapny { get; set; }
+        public Address Address { get; set; }
+        public ICollection<Team> Teams{get; set;}
+
+        public Company() {
+
+        }
+
+        public Company(string companyName, int sizeComapny, Address address, ICollection<Team> teams) {
+            this.CompanyName = companyName;
+            this.SizeOfComapny = sizeComapny;
+            this.Address = address;
+            this.Teams = teams;
+        }
     }
 }
