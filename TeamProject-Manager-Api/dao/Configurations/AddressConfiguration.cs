9a279bb6ad0cb23caf1b03e2cef598d584f8e375
ksciukmateusz <ksciukmateusz@gmail.com>
2021-04-18 18:10:52 +0200

    Creation of DbContext

---
+++ TeamProject-Manager-Api/dao/Configurations/AddressConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TeamProject_Manager_Api.dao.Entitys;
+
+namespace TeamProject_Manager_Api.dao.Configurations {
+    public class AddressConfiguration : IEntityTypeConfiguration<Address> {
+        public void Configure(EntityTypeBuilder<Address> builder) {
+            //to do
+        }
+    }
+}
---
+++ TeamProject-Manager-Api/dao/ProjectManagerDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TeamProject_Manager_Api.dao.Entitys;
+
+namespace TeamProject_Manager_Api.dao {
+    public class ProjectManagerDbContext : DbContext {
+
+        public ProjectManagerDbContext(DbContextOptions<ProjectManagerDbContext> options) : base(options) {
+
+        }
+
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+
+            //implementation
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
