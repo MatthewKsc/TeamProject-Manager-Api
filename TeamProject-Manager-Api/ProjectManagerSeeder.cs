e3723404365cb11c87ee80bef509f26aff042233
ksciukmateusz <ksciukmateusz@gmail.com>
2021-04-22 16:13:43 +0200

    Basic Implementation of Seeder with data + changes in launchSettings

--- TeamProject-Manager-Api/ProjectManagerSeeder.cs
+++ TeamProject-Manager-Api/ProjectManagerSeeder.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using TeamProject_Manager_Api.dao;
+using TeamProject_Manager_Api.dao.Entitys;
 
 
 //to fill up database with data
@@ -16,7 +17,75 @@ namespace TeamProject_Manager_Api {
         }
 
         public void Seed() {
-            
+            if (context.Database.CanConnect()) {
+                if (!context.Companies.Any()) {
+                    var companies = CreatComapny();
+
+                    context.Companies.AddRange(companies);
+                    context.SaveChanges();
+                }
+            }
         }
-    }
+
+        private IEnumerable<Company> CreatComapny() {
+
+            var comapnies = new List<Company>() {
+                new Company() {
+                    CompanyName ="SentorioSTR",
+                    SizeOfComapny = 1500,
+                    Address = DefaultAddress(),
+                    Teams = CreateTeams()
+                }
+            };
+
+            return comapnies;
+        }
+
+        private ICollection<Team> CreateTeams() {
+            var teams = new List<Team>() {
+                new Team(){
+                    NameOfTeam="Oracle dev", 
+                    TeamMembers= new List<User>(){
+                        CreatUser("Matt", "Shein"), 
+                        CreatUser("Serain", "Alento")
+                    },
+                    Projects= DefaulProcjet()
+                },
+                new Team(){
+                    NameOfTeam=".NET dev",
+                    TeamMembers= new List<User>() {
+                        CreatUser("Kaufo", "Cheinz"),
+                        CreatUser("Serafine", "Som"),
+                    },
+                    Projects = DefaulProcjet()
+                },
+            };
+
+            return teams;
+        }
+
+        private User CreatUser(string firstName, string lastName) {
+            return new User() {
+                FirstName = firstName,
+                LastName = lastName,
+                Email = $"{firstName}.{lastName}@str.com",
+                Address = DefaultAddress(),
+                DateOfBirth = DateTime.Parse("1990-01-01")
+            };
+        }
+
+        //only need to fill up basic data in api
+        private Address DefaultAddress() {
+            return new Address() { Country = "Poland", City = "Katowice", Street = "Katowicka", PostalCode = "41-723" };
+        }
+
+        private List<Project> DefaulProcjet() {
+            return new List<Project>() {
+                new Project() {
+                    Title = "Maintain of applications",
+                    Description = "Default job to maintain application from users that already are deployed"
+                }
+            };
+        }
+    } 
 }
