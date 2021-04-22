4f3c3f987c2ac92c402a1581150ec2298c0a24c6
ksciukmateusz <ksciukmateusz@gmail.com>
2021-04-22 15:05:51 +0200

    Seeder Init Model

---
+++ TeamProject-Manager-Api/ProjectManagerSeeder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+//to fill up database with data
+namespace TeamProject_Manager_Api {
+    public class ProjectManagerSeeder {
+
+
+        
+    }
+}
