1455b92b2ddb44cb5c1b47e2cd4ebc3db433e197
ksciukmateusz <ksciukmateusz@gmail.com>
2021-04-23 20:10:03 +0200

    Basic Compnay Controller

---
+++ TeamProject-Manager-Api/Controllers/ComapnyController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeamProject_Manager_Api.dao;
+
+namespace TeamProject_Manager_Api.Controllers
+{
+
+    [ApiController] //allows automatic validaction of models
+    [Route("api/companies")]
+    public class ComapnyController : ControllerBase{
+
+        private readonly ProjectManagerDbContext context;
+
+        //for now to test
+        public ComapnyController(ProjectManagerDbContext context) {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public ActionResult GetAllComapnies() {
+            return Ok(
+                context
+                    .Companies
+                    .Include(c=> c.Address)
+                    .Include(c=> c.Teams)
+                    .ToList()
+            );
+        }
+    }
+}
