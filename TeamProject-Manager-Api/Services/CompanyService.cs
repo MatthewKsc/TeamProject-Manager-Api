b33165fe343af51b458e66b5cf780cfab69d1208
ksciukmateusz <ksciukmateusz@gmail.com>
2021-04-26 15:41:08 +0200

    Company Service Impl

--- TeamProject-Manager-Api/Services/CompanyService.cs
+++ TeamProject-Manager-Api/Services/CompanyService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,16 +12,53 @@ namespace TeamProject_Manager_Api.Services
     public interface ICompanyService {
 
         List<Company> GetAllComapnies();
-        Company GetComapnyById();
-        int CreateCompany();
-        bool DeleteComapnyById();
+        Company GetComapnyById(int Id);
+        int CreateCompany(Company company);
+        bool DeleteComapnyById(int Id);
     }
 
     public class CompanyService : ICompanyService{
 
-        
-        
+        private readonly ProjectManagerDbContext context;
 
+        public CompanyService(ProjectManagerDbContext context) {
+            this.context = context;
+        }
 
+        public List<Company> GetAllComapnies() {
+
+            List<Company> companies= context.Companies
+                .Include(c => c.Address)
+                .Include(c => c.Teams)
+                    .ThenInclude(t => t.Projects)
+                .Include(c => c.Teams)
+                     .ThenInclude(t => t.TeamMembers)
+                     .ToList();
+
+            return companies;
+        }
+
+        public Company GetComapnyById(int Id) {
+            Company company = context.Companies.SingleOrDefault(c => c.Id == Id);
+
+            return company;
+        }
+
+        public int CreateCompany(Company company) {
+            context.Companies.Add(company);
+            context.SaveChanges();
+
+            return company.Id;
+        }
+
+        public bool DeleteComapnyById(int Id) {
+            Company company = context.Companies.SingleOrDefault(c => c.Id == Id);
+
+            if(company is null) {
+                return false;
+            }
+
+            return true;
+        }
     }
 }
