6b49b094c24f0a68a82b4a46b4a2663c9f6d0543
ksciukmateusz <ksciukmateusz@gmail.com>
2021-04-26 14:44:55 +0200

    ICompanyService Implementation

--- TeamProject-Manager-Api/Services/CompanyService.cs
+++ TeamProject-Manager-Api/Services/CompanyService.cs
@@ -3,15 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TeamProject_Manager_Api.dao;
+using TeamProject_Manager_Api.dao.Entitys;
 
 namespace TeamProject_Manager_Api.Services
 {
     public interface ICompanyService {
 
+        List<Company> GetAllComapnies();
+        Company GetComapnyById();
+        int CreateCompany();
+        bool DeleteComapnyById();
     }
 
-    public class CompanyService : ICompanyService
-    {
+    public class CompanyService : ICompanyService{
+
         
+        
+
+
     }
 }
