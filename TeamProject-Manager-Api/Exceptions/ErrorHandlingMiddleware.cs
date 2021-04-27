cb7f4ea607ccf5ae7a51f92a825282fdc205ffa7
ksciukmateusz <ksciukmateusz@gmail.com>
2021-04-27 17:44:03 +0200

    ErrorHandlingMiddleware init class

---
+++ TeamProject-Manager-Api/Exceptions/ErrorHandlingMiddleware.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamProject_Manager_Api.Exceptions
+{
+    public class ErrorHandlingMiddleware : IMiddleware {
+       public async Task InvokeAsync(HttpContext context, RequestDelegate next) {
+            
+       }
+    }
+}
