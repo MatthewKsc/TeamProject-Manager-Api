4297e6ab3f4be696f54fd47ab776bdaea2a1cf8a
ksciukmateusz <ksciukmateusz@gmail.com>
2021-04-27 17:35:59 +0200

    Basic BadRequestException class

---
+++ TeamProject-Manager-Api/Exceptions/BadRequestException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamProject_Manager_Api.Exceptions
+{
+    public class BadRequestException : Exception {
+        public BadRequestException(string message) : base(message) {
+
+        }
+    }
+}
