2356718ebbb3500d7dd38b5cde070c918aae17f7
ksciukmateusz <ksciukmateusz@gmail.com>
2021-04-27 17:47:55 +0200

    ErrorHandlingMiddleware impl

--- TeamProject-Manager-Api/Exceptions/ErrorHandlingMiddleware.cs
+++ TeamProject-Manager-Api/Exceptions/ErrorHandlingMiddleware.cs
@@ -5,11 +5,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TeamProject_Manager_Api.Exceptions
-{
+namespace TeamProject_Manager_Api.Exceptions {
     public class ErrorHandlingMiddleware : IMiddleware {
-       public async Task InvokeAsync(HttpContext context, RequestDelegate next) {
-            
-       }
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next) {
+
+            try {
+                await next.Invoke(context);
+            }
+            catch(BadRequestException badrequest) {
+                context.Response.StatusCode = 400;
+                await context.Response.WriteAsync(badrequest.Message);
+            }
+            catch(NotFoundException notfound) {
+                context.Response.StatusCode = 404;
+                await context.Response.WriteAsync(notfound.Message);
+            }
+            catch(Exception exception) {
+                context.Response.StatusCode = 500;
+                await context.Response.WriteAsync("Server internal error")
+            }
+        }
     }
 }
