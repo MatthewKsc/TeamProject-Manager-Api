9b08374a82c681405de7e00a6a32202150b4575c
ksciukmateusz <ksciukmateusz@gmail.com>
2021-04-28 22:34:08 +0200

    Added Project and User DTO

---
+++ TeamProject-Manager-Api/Dtos/Models/ProjectDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamProject_Manager_Api.Dtos.Models
+{
+    public class ProjectDTO{
+
+        public string ResponsibleTeam { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime? StartOfProject { get; set; }
+        public DateTime? EndOfProject { get; set; }
+        public ICollection<UserDTO> UsersAssigned { get; set; }
+    }
+}
