8eefaf9e0a8d26dff64e1cf27c2af44d27b4c4a1
ksciukmateusz <ksciukmateusz@gmail.com>
2021-05-02 13:24:03 +0200

    Added FluentValidator to API

---
+++ TeamProject-Manager-Api/Dtos/Validators/CompanyValidator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using TeamProject_Manager_Api.Dtos.Models_Operations;
+
+namespace TeamProject_Manager_Api.Dtos.Validators
+{
+    public class CompanyValidator : AbstractValidator<CreateCompany>{
+        
+    }
+}
--- TeamProject-Manager-Api/Startup.cs
+++ TeamProject-Manager-Api/Startup.cs
@@ -1,3 +1,4 @@
+using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,7 +27,7 @@ namespace TeamProject_Manager_Api {
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) {
 
-            services.AddControllers();
+            services.AddControllers().AddFluentValidation();
             services.AddAutoMapper(this.GetType().Assembly);
 
             services.AddScoped<ICompanyService, CompanyService>();
