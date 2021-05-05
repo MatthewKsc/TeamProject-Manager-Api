19af45cdddf49da52692a5bd6eaeb06959d9081a
ksciukmateusz <ksciukmateusz@gmail.com>
2021-05-05 19:44:10 +0200

    Moving registration of services in API container to seperate classes/folder

---
+++ TeamProject-Manager-Api/Installer/IInstaller.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamProject_Manager_Api.Installer
+{
+    public interface IInstaller
+    {
+        void InstallServices(IServiceCollection services, IConfiguration configuration);
+    }
+}
