86a60eb875963be9b3f694af85d8118b0e2a7ded
ksciukmateusz <ksciukmateusz@gmail.com>
2021-05-06 16:56:16 +0200

    Implementation of logger in API to log all Exceptions to proper files

--- TeamProject-Manager-Api/Exceptions/ErrorHandlingMiddleware.cs
+++ TeamProject-Manager-Api/Exceptions/ErrorHandlingMiddleware.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,22 +8,32 @@ using System.Threading.Tasks;
 
 namespace TeamProject_Manager_Api.Exceptions {
     public class ErrorHandlingMiddleware : IMiddleware {
+        
+        private readonly ILogger<ErrorHandlingMiddleware> logger;
+
+        public ErrorHandlingMiddleware(ILogger<ErrorHandlingMiddleware> logger) {
+            this.logger = logger;
+        }
+
         public async Task InvokeAsync(HttpContext context, RequestDelegate next) {
 
             try {
                 await next.Invoke(context);
             }
             catch(BadRequestException badrequest) {
+                logger.LogWarning(badrequest, badrequest.Message);
                 context.Response.StatusCode = 400;
                 await context.Response.WriteAsync(badrequest.Message);
             }
             catch(NotFoundException notfound) {
+                logger.LogWarning(notfound, notfound.Message);
                 context.Response.StatusCode = 404;
                 await context.Response.WriteAsync(notfound.Message);
             }
             catch(Exception exception) {
+                logger.LogError(exception, exception.Message);
                 context.Response.StatusCode = 500;
-                await context.Response.WriteAsync($"Server internal error : {exception.Message}");
+                await context.Response.WriteAsync($"Server internal error");
             }
         }
     }
