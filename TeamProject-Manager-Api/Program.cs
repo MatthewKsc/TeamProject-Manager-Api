baf17717656903f048003adfd57489d7d74b0cf5
ksciukmateusz <ksciukmateusz@gmail.com>
2021-05-06 16:32:20 +0200

    Register Nlog in program class to use it in API

--- TeamProject-Manager-Api/Program.cs
+++ TeamProject-Manager-Api/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using NLog.Web;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,6 +18,7 @@ namespace TeamProject_Manager_Api {
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder => {
                     webBuilder.UseStartup<Startup>();
-                });
+                })
+                .UseNLog();
     }
 }
